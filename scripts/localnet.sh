ba3107bdd73e51da56d98d4d634a55b895c64a5e
edsonalcala <edson_alcala@msn.com>
2024-03-26 17:08:06 -0600

    test

--- scripts/localnet.sh
+++ scripts/localnet.sh
@@ -35,9 +35,9 @@ alice_start=(
 	--alice
 	--port 30334
 	--ws-port 9946
-	--ws-external
 	--rpc-port 9934
-	--rpc-external
+	--unsafe-rpc-external 
+	--unsafe-ws-external
 	--validator
 	--rpc-cors=all
 	--allow-private-ipv4
@@ -51,9 +51,9 @@ bob_start=(
 	--bob
 	--port 30335
 	--ws-port 9947
-	--ws-external
 	--rpc-port 9935
-	--rpc-external
+	--unsafe-rpc-external 
+	--unsafe-ws-external
 	--validator
 	--allow-private-ipv4
 	--discover-local
